e2b20c78dee08a89337385bf5b77df44e92bacf1
Vladyslav <kytsevolov@gmail.com>
2020-09-10 18:38:39 -0600

    bug fix

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,7 +4,7 @@ $('#letter-count').text(maxChars);
 $('[data-tweet-text]').on('input', ({ target }) => {
   $('.tweet-text-error').hide();
   let charCount = maxChars - target.value.length;
-  if (charCount > 0) {
+  if (charCount >= 0) {
     $('#letter-count').text(charCount).css({ color: 'black' });
   } else {
     $('#letter-count').text(charCount).css({ color: 'red' });
